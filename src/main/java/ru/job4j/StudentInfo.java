231235a74d4fa616701bde127fcbd5f2267bd2c4
Vasiliy Makhinya <mavn81@mail.ru>
2022-08-06 21:48:23 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Petr Arsentev");
         System.out.println("06.01.2019");
     }
