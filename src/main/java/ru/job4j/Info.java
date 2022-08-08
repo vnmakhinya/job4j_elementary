6e44b40f2058c0bc3503f936fc7e1f360edc9748
Vasiliy Makhinya <mavn81@mail.ru>
2022-08-08 10:24:19 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,8 @@
-package ru.job4j.info;
+package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("07.08.2022");
+        System.out.println("08.08.2022");
     }
+
 }
