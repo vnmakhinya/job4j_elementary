459b3568d6ae3f5db204c0ad6973d52d7e811749
Vasiliy Makhinya <mavn81@mail.ru>
2022-09-06 20:55:06 +0300

    5.0.1. Сумма чисел. [#226416]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -13,6 +13,8 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+        System.out.println(sum(2, 6));
+        System.out.println(sum(6, 6));
     }
 }
 
