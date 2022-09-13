1199601cdba488c1e9eece1d04833dbeebdc4dd7
Vasiliy Makhinya <mavn81@mail.ru>
2022-09-14 00:29:57 +0300

    5.6. Простые числа [#156314 #342645]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,6 +1,7 @@
 package ru.job4j.loop;
 
 public class CheckPrimeNumber {
+
     public static boolean check(int number) {
         boolean prime = number > 1;
         for (int i = 2; i < number; i++) {
@@ -9,6 +10,6 @@ public class CheckPrimeNumber {
                 break;
             }
         }
-        return prime;
+       return prime;
     }
 }
